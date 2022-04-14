a7cbbeb8eaf90797f9fa87314493188a931b6c91
kevinrakholiya <kevinrakholiya1692001@gmail.com>
2022-04-14 10:30:24 +0530

    Working on Login api

---
+++ Backend/GiellyGreenTeam1/Controllers/LogInController.cs
@@ -0,0 +1,37 @@
+﻿using GiellyGreenTeam1.Helper;
+using GiellyGreenTeam1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GiellyGreenTeam1.Controllers
+{
+    public class LogInController : ApiController
+    {
+        public JsonResponse LogIn(LogIn model)
+        {
+            var objectResponse = new JsonResponse();
+            string userName = System.Web.Configuration.WebConfigurationManager.AppSettings["ApiUserName"];
+            string userPassword = System.Web.Configuration.WebConfigurationManager.AppSettings["ApiPassword"];
+            try
+            {
+                if (model.Email == userName && model.Password == userPassword)
+                {
+                    objectResponse = JsonResponseHelper.JsonMessage(1, "Login Successfully", "Welcome " + userName);
+                }
+                else
+                {
+                    objectResponse = JsonResponseHelper.JsonMessage(1, "Email and password not match", null);
+                }
+            }
+            catch(Exception ex)
+            {
+                objectResponse = JsonResponseHelper.JsonMessage(1, "Error", ex.Message);
+            }
+            return objectResponse;
+        }
+    }
+}
---
+++ Backend/GiellyGreenTeam1/Helper/JsonResponseHelper.cs
@@ -0,0 +1,22 @@
+﻿using GiellyGreenTeam1.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiellyGreenTeam1.Helper
+{
+    public class JsonResponseHelper
+    {
+        public static JsonResponse JsonMessage(int ResponseStatus, string Message, dynamic Result) 
+        {
+            var objectResponse = new JsonResponse();
+            objectResponse.ResponseStatus = ResponseStatus;
+            objectResponse.Message = Message;
+            objectResponse.Result = Result;
+
+            return objectResponse;
+        }
+    }
+}
